ce308ef2a96ef781dc3ef688f5693a90cabeac1b
Paulo Henrique de Andrade <pha.henrique23@gmail.com>
2025-06-02 17:33:01 -0300

    Portfólio web

---
+++ layout/footer.php
@@ -0,0 +1,11 @@
+<footer class="text-center mt-4">
+    <p>&copy; <?php echo date("Y"); ?> Feito por Paulo Henrique de Andrade. Doações pelo Pix 33073785892.</p>
+    <div>
+        <a href="https://www.instagram.com" target="_blank">Instagram</a> |
+        <a href="https://www.facebook.com" target="_blank">Facebook</a> |
+        <a href="https://www.twitter.com" target="_blank">Twitter</a>
+    </div>
+    
+</footer>
+</body>
+</html
